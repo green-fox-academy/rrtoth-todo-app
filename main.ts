be3e644e8341b58111abe0e1b80cbd7b2304aced
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-03 12:14:55 +0200

    empty list

--- main.ts
+++ main.ts
@@ -9,5 +9,5 @@ let a: string[] = todoList.split('\n')
 if (!process.argv[2]) {
     console.log(usageInfo)
 } else if (process.argv[2] == '-l') {
-    a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e)})
+    todoList == ''? console.log("No todos for today! :)") : a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e) })
 }
