f2e266057e00efc5e139354d36cce52cc30b41cd
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-03 12:24:52 +0200

    add new task

--- main.ts
+++ main.ts
@@ -9,5 +9,7 @@ let a: string[] = todoList.split('\n')
 if (!process.argv[2]) {
     console.log(usageInfo)
 } else if (process.argv[2] == '-l') {
-    todoList == ''? console.log("No todos for today! :)") : a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e) })
+    todoList == '' ? console.log("No todos for today! :)") : a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e) })
+} else if (process.argv[2] == '-a') {
+    !process.argv[3] ? console.log(usageInfo) : fs.appendFileSync("todo-list.txt", "\n" + process.argv[3], "utf-8")
 }
