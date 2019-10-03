8f82e6ad05e1f9f16bb94b90d17058b0b0fa1a6d
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-03 16:42:34 +0200

    check task error handling/missing index, index not found, invalid arg type

--- main.ts
+++ main.ts
@@ -5,41 +5,56 @@ const fs = require('fs');
 let usageInfo: string = fs.readFileSync("usage-info.txt", "utf-8")
 let todoList: string = fs.readFileSync("todo-list.txt", "utf-8")
 let a: string[] = todoList.split('\n')
-let args: string[] = ['-l', '-a', '-r', '-c']
+const args: string[] = ['-l', '-a', '-r', '-c']
+let arg1 = process.argv[2]
+let arg2 = process.argv[3]
 
-if (!process.argv[2]) {
+function writeTodo() {
+    fs.writeFileSync("todo-list.txt", a.join('\n'), "utf-8")
+}
+
+// let err = {
+//     'noI': 'a',
+//     'outOfBound': 'Unable to remove: index is out of bound',
+//     'iNotNum': 'Unable to remove: index is not a number'
+// }
+
+if (!arg1) {
     console.log(usageInfo)
-} else if (process.argv[2] == '-l') {
+} else if (arg1 == '-l') {
     todoList == '' ? console.log("No todos for today! :)") : a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e) })
-} else if (process.argv[2] == '-a') {
-    !process.argv[3] ? console.log("Unable to add: no task provided") : fs.appendFileSync("todo-list.txt", "\n" + process.argv[3], "utf-8")
-} else if (process.argv[2] == '-r') {
-    if (!process.argv[3]) {
+} else if (arg1 == '-a') {
+    !arg2 ? console.log("Unable to add: no task provided") : fs.appendFileSync("todo-list.txt", "\n" + arg2, "utf-8")
+} else if (arg1 == '-r') {
+    if (!arg2) {
         console.log('Unable to remove: no index provided');
-    } else if (parseInt(process.argv[3]) > a.length || parseInt(process.argv[3]) < 1) {
+    } else if (parseInt(arg2) > a.length || parseInt(arg2) < 1) {
         console.log('Unable to remove: index is out of bound')
-    } else if (isNaN(parseInt(process.argv[3]))) {
+    } else if (isNaN(parseInt(arg2))) {
         console.log('Unable to remove: index is not a number')
     } else {
-        a.splice(parseInt(process.argv[3]) - 1, 1)
-        fs.writeFileSync("todo-list.txt", a.join('\n'), "utf-8")
+        a.splice(parseInt(arg2) - 1, 1)
+        writeTodo()
     }
-} else if (process.argv[2] == '-c') {
-    if (a[0].substring(0, 1) == '[') {
-        a[parseInt(process.argv[3]) - 1] = "[x]" + a[parseInt(process.argv[3]) - 1].substring(3)
-        fs.writeFileSync("todo-list.txt", a.join('\n'), "utf-8")
-        console.log(a[0].substring(0, 0))
+} else if (arg1 == '-c') {
+    if (!arg2) {
+        console.log('Unable to check: no index provided')
+    } else if (parseInt(arg2) > a.length || parseInt(arg2) < 1) {
+        console.log('Unable to check: index is out of bound')
+    } else if (isNaN(parseInt(arg2))) {
+        console.log('Unable to check: index is not a number')
     } else {
-        for (let i = 0; i < a.length; i++) {
-            a[i] = "[ ] " + a[i]
-        }
-
-        if (a.length >= 2) {
-            a[parseInt(process.argv[3]) - 1] = "[x]" + a[parseInt(process.argv[3]) - 1].substring(3)
-            fs.writeFileSync("todo-list.txt", a.join('\n'), "utf-8")
+        if (a[0].substring(0, 1) == '[') {
+            a[parseInt(arg2) - 1] = "[x]" + a[parseInt(arg2) - 1].substring(3)
+            writeTodo()
+        } else {
+            for (let i = 0; i < a.length; i++) {
+                a[i] = "[ ] " + a[i]
+            }
+            a[parseInt(arg2) - 1] = "[x]" + a[parseInt(arg2) - 1].substring(3)
+            writeTodo()
         }
     }
-
-} else if (!args.some(e => { e == process.argv[2] })) {
+} else if (!args.some(e => { e == arg1 })) {
     console.log('Unsupported argument\n\n' + usageInfo)
 }
