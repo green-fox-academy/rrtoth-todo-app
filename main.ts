ad73d4b3596f4ccd6f3d2f73c6736539fc138543
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-03 14:19:55 +0200

    remove task & error handling/1

--- main.ts
+++ main.ts
@@ -12,4 +12,11 @@ if (!process.argv[2]) {
     todoList == '' ? console.log("No todos for today! :)") : a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e) })
 } else if (process.argv[2] == '-a') {
     !process.argv[3] ? console.log("Unable to add: no task provided") : fs.appendFileSync("todo-list.txt", "\n" + process.argv[3], "utf-8")
+} else if (process.argv[2] == '-r') {
+    if (!process.argv[3]) {
+        console.log('Unable to remove: no index provided');
+    } else {
+        a.splice(parseInt(process.argv[3]) - 1, 1)
+        fs.writeFileSync("todo-list.txt", a.join('\n'), "utf-8")
+    }
 }
