01ae9782e9764423059bab546d9b20f799bd135b
rekatoth <reka.rebeka.toth@gmail.com>
2019-10-03 12:10:15 +0200

    list tasks

--- main.ts
+++ main.ts
@@ -3,7 +3,11 @@
 const fs = require('fs');
 
 let usageInfo: string = fs.readFileSync("usage-info.txt", "utf-8")
+let todoList: string = fs.readFileSync("todo-list.txt", "utf-8")
+let a: string[] = todoList.split('\n')
 
-if (!process.argv[2]){
+if (!process.argv[2]) {
     console.log(usageInfo)
+} else if (process.argv[2] == '-l') {
+    a.forEach(e => { console.log((a.indexOf(e) + 1) + " - " + e)})
 }
